d4325dffaf56751a417a3e824c12aebe186c3940
Innei <tukon479@gmail.com>
2019-07-16 21:05:38 +0800

    07-16

--- express-demo/index.js
+++ express-demo/index.js
@@ -14,7 +14,7 @@ const Names = mongoose.model(
 
 const app = new express();
 
-app.use(express.json()) // 请求处理 json 数据 处理 json 时需要,不加这种中间件, req.body 为 undefined
+app.use(express.json()); // 请求处理 json 数据 处理 json 时需要,不加这种中间件, req.body 为 undefined
 
 //app.use("/", express.static("./public"));
 
@@ -39,8 +39,25 @@ app.get("/names/:name", async (req, res) => {
 app.post("/names", async (req, res) => {
   const data = req.body;
   // res.send(await Names.insertMany(data));
-  res.send(await Names.create(data))
+  res.send(await Names.create(data));
 });
+
+app.put("/names/:name", async (req, res) => {
+  const params = req.params.name;
+  const name = await Names.findOne({ name: params }); // find 接受一个对象 键: 数据库中的字段 值: 需要查找的字段
+  name.name = req.body.name;
+  await name.save();
+  res.send(name);
+  
+});
+
+app.delete('/names/:name', async (req, res) => {
+  const name = await Names.findOne({name: req.params.name})
+  await name.remove()
+  res.send({
+    success: true
+  })
+})
 
 app.listen(8000, () => {
   console.log("server on http://127.0.0.1:8000");
